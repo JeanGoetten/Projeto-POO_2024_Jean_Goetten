0851ce3bc375c387a217557ae32c607724f5c95d
@JeanGoetten <selbestdenkerdot@hotmail.com>
2024-02-23 14:51:23 -0300

    hello world

---
+++ POO/src/Main.java
@@ -0,0 +1,10 @@
+import static java.lang.System.out;
+
+public class Main {
+    public static void main(String[] args) {
+        String hello = "Hello...";
+        out.println(hello);
+        hello = "World!";
+        out.println(hello);
+    }
+}
