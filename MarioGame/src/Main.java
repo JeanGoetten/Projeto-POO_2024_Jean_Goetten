c7211b94bcf3f162953dc8224ed7d3bf68f738d3
@JeanGoetten <selbestdenkerdot@hotmail.com>
2024-03-02 23:43:59 -0300

    add leadership list output

--- MarioGame/src/Main.java
+++ MarioGame/src/Main.java
@@ -9,23 +9,29 @@ a) Adicionar um novo jogador com seu nome, pontuação inicial e as quantidades
 de moedas coletadas em cada nível.
 b) Atualizar a pontuação de um jogador após a conclusão de um nível.
 c) Mostrar a pontuação total de um jogador.
-        d) Mostrar o total de moedas coletadas em um determinado nível por todos os
+d) Mostrar o total de moedas coletadas em um determinado nível por todos os
 jogadores.
-        e) Mostrar o jogador com a pontuação mais alta.
-        ** Considere que o número de jogadores e o número de níveis são fixos.
+e) Mostrar o jogador com a pontuação mais alta.
+** Considere que o número de jogadores e o número de níveis são fixos.
 */
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.*;
 
 public class Main {
     //Variáveis
     static List<String> poolPlayersName = new ArrayList<>();
     static int maxPlayers = 5;
-    static int totalScorePlayer = 0;
-    static int[][] scorePlayerPerLevel = new int[5][3];
-    static int[] maxScorePerLevel = {5, 15, 30};
+    static int totalEnemiesPlayer = 0;
+    static int totalCoinsPlayer = 0;
+    static int[] totalEnemiesPerPlayer = {0, 0, 0, 0, 0};
+    static int[] totalCoinsPerPlayer = {0, 0, 0, 0, 0};
+    static Integer[] finalScorePerPlayer = {0, 0, 0, 0, 0};
+    static int[][] coinsPlayerPerLevel = new int[5][3];
+    static int[] maxCoinsPerLevel = {5, 15, 30};
+    static int[] maxEnemiesPerLevel = {5, 15, 30};
     static int maxLevel = 3;
     static int level = 0;
     //Funções
@@ -57,6 +63,8 @@ public class Main {
     }
     public static void NewGame(){
         level = 0;
+        totalEnemiesPlayer = 0;
+        totalCoinsPlayer = 0;
         Scanner scan = new Scanner(System.in);
         System.out.println("Crie um nome de personagem: ");
         String playerName = scan.nextLine();
@@ -68,29 +76,37 @@ public class Main {
 
                 Play(playerName);
             }
+            if((level + 1) > maxLevel){
+                System.out.println("Parabéns! Você completou a aventura!");
+                totalCoinsPerPlayer[poolPlayersName.size() - 1] = totalCoinsPlayer; //registra na lista o total de moedas coletadas após a aventura
+                totalEnemiesPerPlayer[poolPlayersName.size() - 1] = totalEnemiesPlayer; //registra na lista o total de inimigos abatidos após a aventura
+            }
         }
     }
     public static void Play(String playerName){
         // sorteia aleatoriamente um valor de score
         Random rand = new Random();
-        int rNumber = rand.nextInt(maxScorePerLevel[level] + 1);
-        if(rNumber == 0){ // se o valor do score for igual a 0, a aventura falhou e o jogador a repetirá
-            System.out.println("Você obteve " + rNumber + " pontos e sua aventura falhou! Por favor tente novamente!");
+        int rNumberCoins = rand.nextInt(maxCoinsPerLevel[level] + 1);
+        int rNumberEnemies = rand.nextInt(maxEnemiesPerLevel[level] + 1);
+        if(rNumberCoins == 0){ // se o valor de moedas coletadas for igual a 0, a aventura falhou e o jogador a repetirá
+            System.out.println("Você obteve " + rNumberCoins + " moedas e sua aventura falhou! Por favor tente novamente!");
             // repetir play
-        }else if(rNumber == maxScorePerLevel[level]){ // se o score for máximo, o jogador passa direto de nível
-            System.out.println("Você obteve " + rNumber + " pontos e sua aventura foi perfeita! Preparando próximo nível!");
+        }else if(rNumberCoins == maxCoinsPerLevel[level]){ // se coletou todas, o jogador passa direto de nível
+            System.out.println("Você obteve " + rNumberCoins + " moedas e matou " +  rNumberEnemies + " inimigos. Aventura perfeita! Preparando próximo nível!");
             level++;
-            totalScorePlayer += rNumber; //incrementar score
-            scorePlayerPerLevel[FindPlayerIndex(playerName)][level - 1] = rNumber; //registra os pontos na coluna player e linha level
+            totalEnemiesPlayer += rNumberEnemies; //incrementar inimigos abatidos
+            totalCoinsPlayer += rNumberCoins; //incrementar moedas coletadas
+            coinsPlayerPerLevel[FindPlayerIndex(playerName)][level - 1] = rNumberCoins; //registra as moedas coletadas na coluna player e linha level
         }else{
             Scanner scan = new Scanner(System.in);
-            System.out.println("Você obteve " + rNumber + " pontos de " + maxScorePerLevel[level] + " nessa aventura. Manter pontuação e continuar (1); Refazer aventura (2)");
+            System.out.println("Você obteve " + rNumberCoins + " moedas de " + maxCoinsPerLevel[level] + " e matou " + rNumberEnemies + " inimigos nessa aventura. Manter pontuação e continuar (1); Refazer aventura (2)");
             int opt = scan.nextInt();
             switch (opt){
                 case 1:
                     level++;
-                    totalScorePlayer += rNumber; //incrementar score
-                    scorePlayerPerLevel[FindPlayerIndex(playerName)][level - 1] = rNumber; //registra os pontos na coluna player e linha level
+                    totalEnemiesPlayer += rNumberEnemies; //incrementar inimigos abatidos
+                    totalCoinsPlayer += rNumberCoins; //incrementar moedas coletadas
+                    coinsPlayerPerLevel[FindPlayerIndex(playerName)][level - 1] = rNumberCoins; //registra as moedas na coluna player e linha level
                     break;
                 case 2:
                     // repetir play
@@ -101,11 +117,29 @@ public class Main {
 
         }
     }
-    public static void Continue(String playerName){
-        //Pega o nome de jogador para verificar até que fase foi ou se quer repetir alguma fase
+    public static void Leadership(){
+        Map<String, Integer> leaderMap = new HashMap<String, Integer>();
+
+        for(int i = 0; i < poolPlayersName.size(); i++){
+            finalScorePerPlayer[i] = totalEnemiesPerPlayer[i] + totalCoinsPerPlayer[i];
+
+            leaderMap.put(poolPlayersName.get(i), finalScorePerPlayer[i]);
+        }
+        leaderMap.entrySet().stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .forEach(System.out::println);
+
+
     }
     public static void Scores(){
-        //Mostra a tabela de scores
+        //Mostra a tabela de scores 1) Nome 2) Pontuação total 3) Moedas por nível
+        for(int i = 0; i < poolPlayersName.size(); i++){
+            System.out.println(poolPlayersName.get(i) + " - " + totalEnemiesPerPlayer[i] + " inimigos mortos");
+            for(int j = 0; j < maxLevel; j++){
+                System.out.println(" - Level " + (j + 1) + ": " + coinsPlayerPerLevel[i][j] + " moedas");
+            }
+            System.out.println(" ");
+        }
     }
     public static void Menu(){
         Scanner scan = new Scanner(System.in);
@@ -113,7 +147,7 @@ public class Main {
         boolean playing = true;
 
         while(playing){
-            System.out.println("1 - New Game; 2 - Continue; 3 - Scoreboard; 4 - Sair");
+            System.out.println("1 - New Game; 2 - Leadership; 3 - Scoreboard; 4 - Sair");
             String opt = scan.next();
 
             //Menu
@@ -122,9 +156,7 @@ public class Main {
                     NewGame();
                     break;
                 case "2":
-                    System.out.println("Insira seu nome de jogador: ");
-                    String playerName = scan.nextLine();
-                    Continue(playerName);
+                    Leadership();
                     break;
                 case "3":
                     Scores();
